8978400dee741422b60737ecb423ba14545822f4
Jackon Yang <i@jackon.me>
2018-12-08 03:08:13 +0800

    test comment feature

--- src/dataci/tests/test_use_data.py
+++ src/dataci/tests/test_use_data.py
@@ -27,3 +27,33 @@ def test_basic_func():
     ]
 
     assert got == expect
+
+
+data_comment_kwargs = (
+    {
+        '#': 'this is a class A record',
+        'input_obj': 'hello',
+        'expect': 'world',
+    },
+    {
+        '#': 'this is a class B record',
+        'input_obj': 'hello',
+        'expect': 666,
+    },
+)
+
+
+def test_comment_func():
+    got = []
+ 
+    @use_data(data_comment_kwargs)
+    def merge(input_obj, expect):
+        got.append('%s--%s' % (input_obj, expect))
+    
+    merge()
+
+    expect = [
+        '%s--%s' % (i['input_obj'], i['expect']) for i in data_simple_2_kwargs
+    ]
+
+    assert got == expect
