17d3e4442289cdabb66fb7e05c364ebd2cd541f2
Jackon Yang <i@jackon.me>
2018-12-08 20:47:46 +0800

    refactor to remove duplicated code when looping through testcases

--- src/dataci/__init__.py
+++ src/dataci/__init__.py
@@ -1,25 +1,32 @@
 import json
 import os
 
+DATA_DIR = 'dt-data'
 
 exclude_keys = [
     '#',  # comments
 ]
 
 
+def apply_data(data, f, filename=None):
+    for tc in data:  # tc is short for testcase
+        try:
+            if isinstance(tc, dict):
+                tc = {k: v for k, v in tc.items() if k not in exclude_keys}
+                f(**tc)
+            else:
+                f(*tc)
+        except AssertionError:
+            print('AssertionError caused by testcase:\n%s' % json.dumps(tc, indent=4))
+            if filename is not None:
+                print('in file: %s' % filename)
+            raise
+
+
 def use_data(data):
     def wrapper(f):
         def _func(*args, **kwargs):
-            for tc in data:  # tc is short for testcase
-                try:
-                    if isinstance(tc, dict):
-                        tc = {k: v for k, v in tc.items() if k not in exclude_keys}
-                        f(**tc)
-                    else:
-                        f(*tc)
-                except AssertionError:
-                    print('AssertionError caused by testcase:\n%s' % json.dumps(tc, indent=4))
-                    raise
+            apply_data(data, f)
         return _func
     return wrapper
 
@@ -27,7 +34,9 @@ def use_data(data):
 def use_json_data(filename):
     def wrapper(f):
         def _func(request, *args, **kwargs):
-            data_file = os.path.join(request.fspath.dirname, 'dt-data', filename)
+            # request is a feature from pytest
+            data_file = os.path.join(request.fspath.dirname, DATA_DIR, filename)
+
             with open(data_file) as fp:
                 try:
                     data = json.load(fp)
@@ -35,16 +44,6 @@ def use_json_data(filename):
                     print('Not a valid JSON file: %s' % data_file)
                     raise
 
-            for tc in data:
-                try:
-                    if isinstance(tc, dict):
-                        tc = {k: v for k, v in tc.items() if k not in exclude_keys}
-                        f(**tc)
-                    else:
-                        f(*tc)
-                except AssertionError:
-                    print('AssertionError caused by testcase:\n%s' % json.dumps(tc, indent=4))
-                    print('in file: %s' % data_file)
-                    raise
+            apply_data(data, f, data_file)
         return _func
     return wrapper
